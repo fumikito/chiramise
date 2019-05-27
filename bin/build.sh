7975dd70e38e5e69d1595dcc2ca06596942921ef
fumikito <guy@hametuha.com>
2019-05-28 03:19:44 +0900

    Add travis files.

---
+++ bin/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -ex
+
+npm install
+npm start
+curl -L https://raw.githubusercontent.com/fumikito/wp-readme/master/wp-readme.php | php
+
+rm -rf node_modules
+rm ./README.md
